7d4f58a3cd2d4c8351ef32c17b0ce227a2369de2
suna-yu <suna99_yu@naver.com>
2024-02-06 17:01:49 +0900

    팝업

---
+++ css/script.js
@@ -0,0 +1,9 @@
+/* vanilla js */
+const pop = document.querySelector('.popup');
+const btn = document.querySelector('.popup > span');
+
+
+btn.onclick = function () {
+    console.log("et");
+    pop.style.display="none";
+}
